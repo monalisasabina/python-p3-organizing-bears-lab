d23c14f0997f533c6103a8036e80f084f5fe032d
Monalisa Onyango <+>
2024-09-10 15:43:11 +0300

    SQL LAB

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate',4,'M','Brown','Aggressive',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Rowdy',10,'M','Brown','Rowdy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Tabitha',3,'F','Blonde','Curious',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Segrgent Brown',20,'M','Black','Defensive',0);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Melissa',9,'F','Blonde','Shy',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Grinch',7,'M','Golden','Aggressive',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES('Wendy',7,'F','Brown','Skittish',1);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES(NULL,2,'M','Brown','Shy',1);
